d298eaa2b594723442fa189043d5faa5e200f603
Patrick Kofod Mogensen <patrick.mogensen@gmail.com>
2016-04-24 15:09:29 +0200

    Yes

---
+++ documentation/build/_sources/derivative_based.rst
@@ -0,0 +1,2 @@
+Derivative Based Methods
+===
---
+++ src/Docs.jl
@@ -0,0 +1,16 @@
+module Docs
+    """
+    This is a function
+    """
+    function add_six_and_two()
+        6+2
+    end
+
+    function subtract_three(x)
+        x-3
+    end
+
+    function subtract_2_or_num(x;num = 2)
+        x - num
+    end
+end
